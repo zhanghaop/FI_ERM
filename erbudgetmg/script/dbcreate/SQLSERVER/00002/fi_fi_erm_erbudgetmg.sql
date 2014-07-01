72fbe941bb656ca70b591215fe1e316dc366cad7
chenshuaia <chenshuaia@yonyou.com>
2014-07-01 09:01:10 +0800

    申请记录表增加索引脚本
    Change-Id: I443faef2411db33db031b723872b3961c9435c12
    Signed-off-by: chenshuaia <chenshuaia@yonyou.com>

--- erbudgetmg/script/dbcreate/SQLSERVER/00002/fi_fi_erm_erbudgetmg.sql
+++ erbudgetmg/script/dbcreate/SQLSERVER/00002/fi_fi_erm_erbudgetmg.sql
@@ -1,5 +1,18 @@
+/* indexcode: I_ERM_PF_MADETAIL */
+create  index I_ERM_PF_MADETAIL on er_mtapp_pf (pk_mtapp_detail)
+go
+
+/* indexcode: I_ERM_PF_BUSIPK */
+create  index I_ERM_PF_BUSIPK on er_mtapp_pf (busi_pk)
+go
+
 /* indexcode: I_ERM_MTDETAIL_ORG_DATE */
 create  index I_ERM_MTDETAIL_ORG_DATE on er_mtapp_detail (pk_org,
+billdate)
+go
+
+/* indexcode: I_ERM_MA_ORG */
+create  index I_ERM_MA_ORG on er_mtapp_bill (pk_org,
 billdate)
 go
 
