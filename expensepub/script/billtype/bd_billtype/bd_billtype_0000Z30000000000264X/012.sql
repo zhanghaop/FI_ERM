07c902debd7231ce3fab2ea86d7ff3e8da6897c0
chenshuaia <chenshuaia@yonyou.com>
2014-09-16 16:08:22 +0800

    635费用下游单据重复问题
    Change-Id: I40c345452911d27794ddb9b8c6546fe8248ee218
    Signed-off-by: chenshuaia <chenshuaia@yonyou.com>

--- expensepub/script/billtype/bd_billtype/bd_billtype_0000Z30000000000264X/012.sql
+++ expensepub/script/billtype/bd_billtype/bd_billtype_0000Z30000000000264X/012.sql
@@ -1,6 +1,3 @@
-insert into bd_fwdbilltype(pk_fwdbilltype,dr,isbizflowbill,iscrossgrp,pk_backbilltype,pk_billtype,pk_billtypeid,ts) values('1001Z31000000000UWNU',0,'Y','N','264X','36J1','0000Z30000000000264X','2013-08-02 09:51:12')
-go
-
 insert into bd_fwdbilltype(pk_fwdbilltype,dr,isbizflowbill,iscrossgrp,pk_backbilltype,pk_billtype,pk_billtypeid,ts) values('1001Z31000000000Y7HG',0,'Y','N','264X','HJ','0000Z30000000000264X','2014-04-29 09:51:48')
 go
 
