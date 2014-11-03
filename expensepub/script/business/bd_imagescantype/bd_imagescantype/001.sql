7dd119bc8129ce4459231cddfc8e2b001b822047
shengqy <shengqy@yonyou.com>
2014-11-03 08:43:56 +0800

    共享服务bd_imagescantype进行了改动
    Change-Id: Ic4b8530372f818c251b6e9840bae7084127fe6a3
    Signed-off-by: shengqy <shengqy@yonyou.com>

--- expensepub/script/business/bd_imagescantype/bd_imagescantype/001.sql
+++ expensepub/script/business/bd_imagescantype/bd_imagescantype/001.sql
@@ -1,18 +1,18 @@
-insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts) values('1001Z0100000000VTPDD','01',0,'集团管控',null,null,null,null)
+insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts,billtypecode) values('1001Z0100000000VTPDD','01',0,'集团管控',null,null,null,null,null)
 go
 
-insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts) values('1001Z3100000000V261X','010101',0,'费用申请单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','261X','1001Z3100000000VTPDE',null)
+insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts,billtypecode) values('1001Z3100000000V261X','010101',0,'费用申请单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','1001Z31000000001DKFG','1001Z3100000000VTPDE',null,'261X')
 go
 
-insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts) values('1001Z3100000000V262X','010102',0,'主预提单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','262X','1001Z3100000000VTPDE',null)
+insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts,billtypecode) values('1001Z3100000000V262X','010102',0,'主预提单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','1001Z310000000018ERQ','1001Z3100000000VTPDE',null,'262X')
 go
 
-insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts) values('1001Z3100000000V263X','010103',0,'主借款单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','263X','1001Z3100000000VTPDE',null)
+insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts,billtypecode) values('1001Z3100000000V263X','010103',0,'主借款单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','0000Z30000000000263X','1001Z3100000000VTPDE',null,'263X')
 go
 
-insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts) values('1001Z3100000000V264X','010104',0,'主报销单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','264X','1001Z3100000000VTPDE',null)
+insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts,billtypecode) values('1001Z3100000000V264X','010104',0,'主报销单','nc.ui.org.ref.FinanceOrgDefaultRefTreeModel','0000Z30000000000264X','1001Z3100000000VTPDE',null,'264X')
 go
 
-insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts) values('1001Z3100000000VTPDE','0101',0,'费用管理',null,null,'1001Z0100000000VTPDD',null)
+insert into bd_imagescantype(pk_id,code,dr,name,orgrefclass,pk_billtypecode,pk_parent,ts,billtypecode) values('1001Z3100000000VTPDE','0101',0,'费用管理',null,null,'1001Z0100000000VTPDD',null,null)
 go
 
