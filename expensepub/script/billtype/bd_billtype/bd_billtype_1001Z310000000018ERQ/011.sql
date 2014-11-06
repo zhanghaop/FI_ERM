c0b460ebad46c70925ed45bb8fb926955fafd011
shengqy <shengqy@yonyou.com>
2014-11-06 10:09:12 +0800

    借款单、预提单限定模式支持项目负责人
    Change-Id: I3f02609083437ce5824596fd9b8f367113f38cfb
    Signed-off-by: shengqy <shengqy@yonyou.com>

--- expensepub/script/billtype/bd_billtype/bd_billtype_1001Z310000000018ERQ/011.sql
+++ expensepub/script/billtype/bd_billtype/bd_billtype_1001Z310000000018ERQ/011.sql
@@ -1,12 +1,15 @@
-insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',6,'nc.ui.er.djlx.AcITranstypeEditorImp',null,null,'1001Z310000000018ERQ','2014-03-13 08:44:05')
+insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',1,'nc.bs.er.wfengine.ext.WfFyFilterAccProject','q',null,'1001Z310000000018ERQ','2014-11-06 08:44:33')
 go
 
-insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',7,'nc.impl.er.djlx.ErmITranstypeImp',null,null,'1001Z310000000018ERQ','2014-03-13 08:44:05')
+insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',6,'nc.ui.er.djlx.AcITranstypeEditorImp',null,null,'1001Z310000000018ERQ','2014-11-06 08:44:33')
 go
 
-insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',9,'nc.ui.erm.accruedexpense.budgetinit.ErmAccPfClientBizProcess',null,null,'1001Z310000000018ERQ','2014-03-13 08:44:05')
+insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',7,'nc.impl.er.djlx.ErmITranstypeImp',null,null,'1001Z310000000018ERQ','2014-11-06 08:44:33')
 go
 
-insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',9,'ssc.ui.wf.engine.SSCPfClientBizProcess',null,null,'1001Z310000000018ERQ','2014-09-15 10:34:56')
+insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',9,'nc.ui.erm.accruedexpense.budgetinit.ErmAccPfClientBizProcess',null,null,'1001Z310000000018ERQ','2014-11-06 08:44:33')
+go
+
+insert into bd_billtype2(pk_billtype,classtype,classname,code,dr,pk_billtypeid,ts) values('262X',9,'ssc.ui.wf.engine.SSCPfClientBizProcess',null,null,'1001Z310000000018ERQ','2014-11-06 08:44:33')
 go
 
