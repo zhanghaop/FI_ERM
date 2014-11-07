f051904aedb3d9a70f18f1f396bdd93eb72ec89b
shengqy <shengqy@yonyou.com>
2014-11-07 16:59:27 +0800

    费用结转单拉报销单单据模板合计脚本
    Change-Id: Ifb3d821bbac4934c1ec5578db0d539af1b9b0bef
    Signed-off-by: shengqy <shengqy@yonyou.com>

---
+++ expensepub/script/billtemplet/pub_billtemplet/pub_billtemplet_1001Z31000000000E0MT/003.sql
@@ -0,0 +1,3 @@
+insert into pub_billtemplet_t(pk_billtemplet_t,basetab,dr,metadataclass,metadatapath,mixindex,pk_billtemplet,pk_layout,pos,position,resid,tabcode,tabindex,tabname,ts,vdef1,vdef2,vdef3) values('1001Z310000000000PGS',null,0,'erm.bxzb',null,null,'1001Z31000000000E0MT','~',0,0,'120110ETEA0015','bxzb',0,'报销单','2014-11-07 16:43:53',null,null,null)
+go
+
