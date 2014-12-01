7b3abc349d5ed64f777f3a8f7d928a4a89c25236
shengqy <shengqy@yonyou.com>
2014-12-01 16:45:04 +0800

    收付款合同升级
    Change-Id: I123d8bf6d17dd672b81780d7722ad50c57a5b317
    Signed-off-by: shengqy <shengqy@yonyou.com>

--- expenseaccount/src/private/nc/impl/erm/common/ErmMDUpdate.java
+++ expenseaccount/src/private/nc/impl/erm/common/ErmMDUpdate.java
@@ -27,23 +27,19 @@ import nc.vo.pub.BusinessException;
  */
 public class ErmMDUpdate extends AbstractPatchInstall {
 	private IPublishService service;
-	
+
 	private BaseDAO dao = null;
 
 	// 元数据如果相互关联，需注意先后顺序
-	private String[] bmfpath = {
-			"/modules/erm/METADATA/accruedexpense.bmf",
-			"/modules/erm/METADATA/reimdimension.bmf",
+	private String[] bmfpath = { "/modules/erm/METADATA/accruedexpense.bmf", "/modules/erm/METADATA/reimdimension.bmf",
 			"/modules/erm/METADATA/reimrule.bmf", "/modules/erm/METADATA/ermcostaccount.bmf",
 			"/modules/erm/METADATA/ermcostaccountbal.bmf", "/modules/erm/METADATA/matterappbill.bmf",
-			"/modules/erm/METADATA/matterappbill_intermonth_ext.bmf", 
-			"/modules/erm/METADATA/expenseaccount.bmf", "/modules/erm/METADATA/loanmanage.bmf" ,
-			"/modules/erm/METADATA/costsharebill.bmf", "/modules/erm/METADATA/expamortizeinfo.bmf",
-			"/modules/erm/METADATA/bxbillmanage.bmf","/modules/erm/METADATA/ermbilltype.bmf" };
+			"/modules/erm/METADATA/matterappbill_intermonth_ext.bmf", "/modules/erm/METADATA/expenseaccount.bmf",
+			"/modules/erm/METADATA/loanmanage.bmf", "/modules/erm/METADATA/costsharebill.bmf",
+			"/modules/erm/METADATA/expamortizeinfo.bmf", "/modules/erm/METADATA/bxbillmanage.bmf",
+			"/modules/erm/METADATA/ermbilltype.bmf" };
 
-	private String[] bpfpath = {
-			"/modules/erm/METADATA/accruedoperate.bpf"
-	};
+	private String[] bpfpath = { "/modules/erm/METADATA/accruedoperate.bpf" };
 
 	/**
 	 * 发布元数据
@@ -76,9 +72,10 @@ public class ErmMDUpdate extends AbstractPatchInstall {
 				throw new BusinessException(ex.getMessage(), ex);
 			}
 		}
-		
+
 		// pub_vochange_b表维护
 		delVOChange_b();
+		updateFct();
 	}
 
 	private void delVOChange_b() throws DAOException {
@@ -86,8 +83,40 @@ public class ErmMDUpdate extends AbstractPatchInstall {
 		getBaseDAO().executeUpdate(sql);
 		Logger.debug(sql);
 	}
-	
-	
+
+	/**
+	 * 已安装收付款合同的情况下才执行。
+	 * 
+	 * @throws BusinessException
+	 * @throws Exception
+	 */
+	private void updateFct() throws BusinessException {
+		try {
+			BaseDAO dao = new BaseDAO();
+			dao.executeUpdate("update fct_ar set dr=dr");
+		} catch (Exception e) {
+			return;
+		}
+		String[] sqls = new String[] {
+				"delete from md_property where id in ('32901b16-b984-43fb-9d9b-148ecabf5be9','78d1da6b-0af5-4ddb-b629-cf8df7e42b04')",
+				"delete from md_db_relation where id in ('130c3ecb-5647-43da-bc5c-2571b0ed1a62','b5d73606-f408-411c-8e0a-c6aeb04c472d')",
+				"insert into md_db_relation (asstype, createtime, creator, description, displayname, dr, endcardinality, endfieldid, endtableid, help, id, isforeignkey, modifier, modifytime, name, resid, startattrid, startcardinality, startfieldid, starttableid, ts, versiontype) values (3, null, null, null, 'jk_busitem_ct_ap', null, null, 'fct_ap@@PK@@', 'fct_ap', null, '130c3ecb-5647-43da-bc5c-2571b0ed1a62', 'N', null, null, 'jk_busitem_ct_ap', null, '32901b16-b984-43fb-9d9b-148ecabf5be9', null, 'er_busitem@@@fctno', 'er_busitem', '2014-11-15 10:00:11', 0)",
+				"insert into md_db_relation (asstype, createtime, creator, description, displayname, dr, endcardinality, endfieldid, endtableid, help, id, isforeignkey, modifier, modifytime, name, resid, startattrid, startcardinality, startfieldid, starttableid, ts, versiontype) values (3, null, null, null, 'er_busitem_fct_ap', null, null, 'fct_ap@@PK@@', 'fct_ap', null, 'b5d73606-f408-411c-8e0a-c6aeb04c472d', 'N', null, null, 'er_busitem_fct_ap', null, '78d1da6b-0af5-4ddb-b629-cf8df7e42b04', null, 'er_busitem@@@fctno', 'er_busitem', '2014-11-15 10:00:10', 0)",
+				"insert into md_property (accessorclassname, accesspower, accesspowergroup, attrlength, attrmaxvalue, attrminvalue, attrsequence, calculation, classid, createindustry, createtime, creator, customattr, datatype, datatypestyle, defaultvalue, description, displayname, dr, dynamicattr, dynamictable, fixedlength, help, hided, id, industry, isactive, isauthen, modifier, modifytime, name, notserialize, nullable, precise, readonly, refmodelname, resid, ts, versiontype, visibility) values (null, 'N', null, 20, null, null, 91, 'N', '6953ec6a-329c-4c09-b950-25b4af68e5c5', '0', null, null, 'N', 'af6a8e77-4fa3-4316-9d8d-12d24a9ff338', 305, null, null, '合同号', null, 'N', null, 'N', null, 'N', '32901b16-b984-43fb-9d9b-148ecabf5be9', '0', 'Y', null, null, null, 'fctno', 'N', 'Y', 0, 'N', '付款合同', '2UC000-000234', '2014-11-15 10:00:11', 0, 0)",
+				"insert into md_property (accessorclassname, accesspower, accesspowergroup, attrlength, attrmaxvalue, attrminvalue, attrsequence, calculation, classid, createindustry, createtime, creator, customattr, datatype, datatypestyle, defaultvalue, description, displayname, dr, dynamicattr, dynamictable, fixedlength, help, hided, id, industry, isactive, isauthen, modifier, modifytime, name, notserialize, nullable, precise, readonly, refmodelname, resid, ts, versiontype, visibility) values (null, 'N', null, 20, null, null, 95, 'N', 'ece96dd8-bdf8-4db3-a112-9d2f636d388f', '0', null, null, 'N', 'af6a8e77-4fa3-4316-9d8d-12d24a9ff338', 305, null, null, '合同号', null, 'N', null, 'N', null, 'N', '78d1da6b-0af5-4ddb-b629-cf8df7e42b04', '0', 'Y', null, null, null, 'fctno', 'N', 'Y', 0, 'N', '付款合同', '2UC000-000234', '2014-11-15 10:00:09', 0, 0)", };
+		for (String sql : sqls) {
+			executeSql(dao, sql);
+		}
+	}
+
+	private void executeSql(BaseDAO dao, String sql) throws BusinessException {
+		try {
+			dao.executeUpdate(sql);
+		} catch (Exception e) {
+			throw new BusinessException("费用升级出错：" + sql);
+		}
+	}
+
 	private BaseDAO getBaseDAO() {
 		if (dao == null) {
 			dao = new BaseDAO();
