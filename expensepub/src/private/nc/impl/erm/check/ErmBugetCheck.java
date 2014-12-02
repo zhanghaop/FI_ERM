51397ec36bef5c856264249e3f8e5d61a7927702
chenshuaia <chenshuaia@yonyou.com>
2014-12-02 16:00:59 +0800

    共享服务预算预警接口
    Change-Id: I6c48954a31b79f6d4f23dc551b1bf3314f1803f2
    Signed-off-by: chenshuaia <chenshuaia@yonyou.com>

--- expensepub/src/private/nc/impl/erm/check/ErmBugetCheck.java
+++ expensepub/src/private/nc/impl/erm/check/ErmBugetCheck.java
@@ -1,10 +1,15 @@
 package nc.impl.erm.check;
 
+import nc.bs.erm.accruedexpense.common.AccFlowCheckInfo;
+import nc.bs.erm.matterapp.common.ErmMtAppFlowCheckInfo;
 import nc.bs.pub.pf.IBusinessCheck;
 import nc.vo.ep.bx.ErmFlowCheckInfo;
 import nc.vo.ep.bx.JKBXVO;
+import nc.vo.erm.accruedexpense.AggAccruedBillVO;
+import nc.vo.erm.matterapp.AggMatterAppVO;
 import nc.vo.pub.BusinessException;
 import nc.vo.pub.BusinessRuntimeException;
+import nc.vo.pub.lang.UFBoolean;
 import nc.vo.tb.control.NtbCtlInfoVO;
 
 public class ErmBugetCheck implements IBusinessCheck {
@@ -12,14 +17,26 @@ public class ErmBugetCheck implements IBusinessCheck {
 	@Override
 	public String check(Object billvo) {
 		try {
-			return getYsControlAlarmInfo(ErmFlowCheckInfo.doNtbCheck((JKBXVO) billvo));
+			if (billvo instanceof JKBXVO) {
+				return getYsControlAlarmInfo(ErmFlowCheckInfo
+						.doNtbCheck((JKBXVO) billvo));
+			}
+			if (billvo instanceof AggMatterAppVO) {
+				return getYsControlAlarmInfo(ErmMtAppFlowCheckInfo
+						.doNtbCheck((AggMatterAppVO) billvo));
+			}
+			if (billvo instanceof AggAccruedBillVO) {
+				return getYsControlAlarmInfo(AccFlowCheckInfo
+						.doNtbCheck((AggAccruedBillVO) billvo));
+			}
+			return null;
 		} catch (BusinessException e) {
 			throw new BusinessRuntimeException(e.getMessage(), e);
 		}
 	}
-	
+
 	private String getYsControlAlarmInfo(NtbCtlInfoVO tpcontrolvo) {
-		if(tpcontrolvo == null){
+		if (tpcontrolvo == null) {
 			return null;
 		}
 		StringBuffer controlMsg = new StringBuffer();
@@ -35,8 +52,18 @@ public class ErmBugetCheck implements IBusinessCheck {
 
 	@Override
 	public Object checkCallBack(Object billvo) {
-		((JKBXVO) billvo).setHasNtbCheck(true);
+		if (billvo instanceof JKBXVO) {
+			((JKBXVO) billvo).setHasNtbCheck(true);
+		}
+		if (billvo instanceof AggMatterAppVO) {
+			((AggMatterAppVO) billvo).getParentVO().setHasntbcheck(UFBoolean.TRUE);
+		}
+		if (billvo instanceof AggAccruedBillVO) {
+			((AggAccruedBillVO) billvo).getParentVO().setHasntbcheck(UFBoolean.TRUE);
+		}
+		
 		return billvo;
 	}
 
 }
+
