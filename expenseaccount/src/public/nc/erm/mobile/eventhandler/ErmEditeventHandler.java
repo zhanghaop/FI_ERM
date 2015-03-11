5a90179557b02793efa6c9052c4ec3879e6b937f
shiwla <shiwla@yonyou.com>
2015-03-11 09:09:10 +0800

    交叉校验规则
    Change-Id: I104f0ed9d361eaa97cdfe5d7d4ea5fb4320a4179
    Signed-off-by: shiwla <shiwla@yonyou.com>

--- expenseaccount/src/public/nc/erm/mobile/eventhandler/ErmEditeventHandler.java
+++ expenseaccount/src/public/nc/erm/mobile/eventhandler/ErmEditeventHandler.java
@@ -2,6 +2,8 @@ package nc.erm.mobile.eventhandler;
 
 import nc.erm.mobile.jkbxeventhandler.HeadEventHandle;
 import nc.vo.arap.bx.util.BXConstans;
+import nc.vo.fipub.billrule.BillRuleItemVO;
+import nc.vo.fipub.rulecontrol.RuleDataCacheEx;
 import nc.vo.pub.BusinessException;
 import nc.vo.pub.SuperVO;
 
@@ -13,12 +15,21 @@ public class ErmEditeventHandler extends AbstractEditeventHandler{
 	public void handleEditevent(JsonVoTransform jsonVoTransform) throws BusinessException{
 		EditItemInfoVO  editinfo = jsonVoTransform.getEditItemInfoVO();
 		if(editinfo.isHead()){
-			//借款报销单表头编辑后处理
+			//表头编辑后处理
+			//借款报销单
 			if(isJkBxBill(jsonVoTransform.getHeadVO()))
 				new HeadEventHandle().process(jsonVoTransform);
+			//处理交叉校验规则
+			CrossCheckUtil.checkRule("Y", editinfo.getId(), null,jsonVoTransform.getHeadVO(),null);
+//			BillRuleItemVO[] br = RuleDataCacheEx.getInstance().getBillBindItems(djlxbm, pk_org, false);
 		}else{
+			//表体编辑后处理
 			//报销标准
 //			new ReimRuleEventHandle().process(jsonVoTransform);
+			//费用申请单表头BillCardHeadAfterEditlistener
+			//费用申请单表体BillCardHeadAfterEditlistener
+			//处理交叉校验规则
+			CrossCheckUtil.checkRule("N", editinfo.getId(), null,jsonVoTransform.getHeadVO(),jsonVoTransform.getBodysMap());
 		}
 	}
 }
